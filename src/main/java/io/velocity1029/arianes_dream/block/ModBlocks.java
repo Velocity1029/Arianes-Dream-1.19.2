ef65bdf85efb99d3e06dcd1ac649df0dd179cc72
Velocity1029 <ben.omeara1029@yahoo.com>
2024-06-06 00:22:14 -0400

    Registry rewrites.

--- src/main/java/io/velocity1029/arianes_dream/block/ModBlocks.java
+++ src/main/java/io/velocity1029/arianes_dream/block/ModBlocks.java
@@ -1,32 +1,40 @@
 package io.velocity1029.arianes_dream.block;
 
+import io.velocity1029.arianes_dream.item.ModItems;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 import static io.velocity1029.arianes_dream.ArianesDream.MODID;
+import static io.velocity1029.arianes_dream.ArianesDream.MOD_TAB;
 
 public class ModBlocks {
     // Create a Deferred Register to hold Items which will all be registered under the "arianes_dream" namespace
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
 
+    public static final RegistryObject<Block> TESSERACT;
+    public static final RegistryObject<Block> DUST_LAYER;
+    public static final RegistryObject<Block> DUST_BLOCK;
+
+    static {
+        TESSERACT = registerBlock("tesseract", () -> new Block(BlockBehaviour.Properties.of(Material.AMETHYST).explosionResistance(100)));
+        DUST_LAYER = registerBlock("dust_layer", () -> new SnowLayerBlock(BlockBehaviour.Properties.of(Material.TOP_SNOW).sound(SoundType.SAND)));
+        DUST_BLOCK = registerBlock("dust_block", () -> new SandBlock(11098145, BlockBehaviour.Properties.of(Material.SAND, MaterialColor.COLOR_RED).strength(0.5F,10).sound(SoundType.SAND)));
+    }
+
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
     }
 
-    // Creates a new BlockItem with the id "arianes_dream:tesseract", combining the namespace and path
-    public static final RegistryObject<Block> TESSERACT = BLOCKS.register("tesseract",
-            () -> new Block(BlockBehaviour.Properties.of(Material.AMETHYST).explosionResistance(100)));
-
-    // Creates a new BlockItem with the id "arianes_dream:tesseract", combining the namespace and path
-    public static final RegistryObject<Block> DUST_LAYER = BLOCKS.register("dust_layer",
-            () -> new SnowLayerBlock(BlockBehaviour.Properties.of(Material.TOP_SNOW).sound(SoundType.SAND)));
-
-
-    public static final RegistryObject<Block> DUST_BLOCK = BLOCKS.register("dust_block",
-            () -> new FallingBlock(BlockBehaviour.Properties.of(Material.SAND).sound(SoundType.SAND)));
+    private static <T extends Block> RegistryObject<T> registerBlock(String pName, Supplier<T> supplier) {
+        return BLOCKS.register(pName, supplier);
+    }
 }
--- src/main/java/io/velocity1029/arianes_dream/item/ModItems.java
+++ src/main/java/io/velocity1029/arianes_dream/item/ModItems.java
@@ -12,6 +12,8 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 import static io.velocity1029.arianes_dream.ArianesDream.MOD_TAB;
 import static io.velocity1029.arianes_dream.ArianesDream.MODID;
 
@@ -19,21 +21,23 @@ public class ModItems {
     // Create a Deferred Register to hold Items which will all be registered under the "arianes_dream" namespace
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
-    // Creates a new BlockItem with the id "arianes_dream:tesseract", combining the namespace and path
-    public static final RegistryObject<Item> TESSERACT = ITEMS.register("tesseract",
-            () -> new Item(new Item.Properties().tab(MOD_TAB)));
-            //() -> new BlockItem(TESSERACT.get(), new Item.Properties().tab(ARIANES_TAB)));
+    public static final RegistryObject<Item> TESSERACT;
+    public static final RegistryObject<Item> DUST_LAYER;
+    public static final RegistryObject<Item> DUST_BLOCK;
+    public static final RegistryObject<Item> METALLIC_DUST;
 
-    public static final  RegistryObject<BlockItem> DUST_LAYER = ITEMS.register("dust_layer",
-            () -> new BlockItem(ModBlocks.DUST_LAYER.get(), new Item.Properties().tab(MOD_TAB)));
-
-    public static final  RegistryObject<BlockItem> DUST_BLOCK = ITEMS.register("dust_block",
-            () -> new BlockItem(ModBlocks.DUST_BLOCK.get(), new Item.Properties().tab(MOD_TAB)));
-
-    public static final RegistryObject<Item> METALLIC_DUST = ITEMS.register("metallic_dust",
-            () -> new Item(new Item.Properties().tab(MOD_TAB)));
+    static {
+        TESSERACT = registerItem("tesseract", () -> new Item(new Item.Properties().tab(MOD_TAB)));//() -> new BlockItem(TESSERACT.get(), new Item.Properties().tab(MOD_TAB)));
+        DUST_LAYER = registerItem("dust_layer", () -> new BlockItem(ModBlocks.DUST_LAYER.get(), new Item.Properties().tab(MOD_TAB)));
+        DUST_BLOCK = registerItem("dust_block", () -> new BlockItem(ModBlocks.DUST_BLOCK.get(), new Item.Properties().tab(MOD_TAB)));
+        METALLIC_DUST = registerItem("metallic_dust", () -> new Item(new Item.Properties().tab(MOD_TAB)));
+    }
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
+    }
+
+    private static <T extends Item> RegistryObject<T> registerItem(String pName, Supplier<T> supplier) {
+        return ITEMS.register(pName, supplier);
     }
 }
